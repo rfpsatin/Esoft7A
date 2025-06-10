9c48045c56b74955e06c70e9f175ed71aa4298e7
kaikdorvalo <kaikdorvalo@gmail.com>
2025-06-09 21:44:36 -0300

    setup

--- README.md
+++
@@ -1,6 +0,0 @@
-integrantes:
-
-- Rhayssa Justino Andretto RA:211631272
-- Yves Morello RA: 220140552
-- Vinicius Kenji Uda RA: 222591332
-- Lorenzo Marzzola RA: 230671792
--- clusterizacao/services/euclidean_distance.py
+++
@@ -1,9 +0,0 @@
-import math
-from typing import Tuple
-
-def euclidean_distance(centroid: Tuple[int, float, float], student: Tuple[int, float, float]) -> float:
-    return math.sqrt(
-        (centroid[0] - student[0])**2 +
-        (centroid[1] - student[1])**2 +
-        (centroid[2] - student[2])**2
-    )
